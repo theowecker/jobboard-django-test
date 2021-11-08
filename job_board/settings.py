4f72d963cf0fb0dec843122ce80a001bacfcd0c0
Theo Wecker <tlwecker@yahoo.com>
2021-11-08 16:47:15 -0700

    main

--- job_board/settings.py
+++ job_board/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-716n339(cau26^a2o)=#$3b)hh=lxqo%1dn%!w#gei4#90pr^e'
+SECRET_KEY = 'banana!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
